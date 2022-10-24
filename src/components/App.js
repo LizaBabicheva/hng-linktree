649231ad216b066b6db977d2a5481025df3fb870
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-24 22:09:41 +0300

    instal react

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+import Profile from './Profile';
+import Links from './Links';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div id="root">
+      <Profile />
+      <Links />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
