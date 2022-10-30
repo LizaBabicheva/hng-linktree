789c911fa3ce6982457af32c9d0d063e1501d561
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-30 23:17:39 +0300

    make tooltips

--- src/components/Links.js
+++ src/components/Links.js
@@ -7,11 +7,21 @@ function Links() {
     return (
         <section id='links' className='links'>
             <div className='links__buttons'>
-                <a id='btn__zuri' className='links__button' href='https://training.zuri.team/'>Zuri Team</a>
-                <a id='books' className='links__button' href='https://books.zuri.team/'>Zuri Books</a>
-                <a id='book__python' className='links__button' href='https://books.zuri.team/python-for-beginners?ref_id=<LizaBabicheva>'>Python Books</a>
-                <a id='pitch' className='links__button' href='https://background.zuri.team/'>Background Check for Coders</a>
-                <a id='book__design' className='links__button' href='https://books.zuri.team/design-rules'>Design Books</a>
+                <a id='btn__zuri' className='links__button' href='https://training.zuri.team/'>Zuri Team
+                    <span className='tooltip'>Meet Zuri Team</span>
+                </a>
+                <a id='books' className='links__button' href='https://books.zuri.team/'>Zuri Books
+                    <span className='tooltip'>Stop scrolling your twitter and go read a book!</span>
+                </a>
+                <a id='book__python' className='links__button' href='https://books.zuri.team/python-for-beginners?ref_id=<LizaBabicheva>'>Python Books
+                    <span className='tooltip'>Want to change your life before 2023? Check this one.</span>
+                </a>
+                <a id='pitch' className='links__button' href='https://background.zuri.team/'>Background Check for Coders
+                    <span className='tooltip'>Learn & earn! Sounds nice, but what about learn & earn remotely?</span>
+                </a>
+                <a id='book__design' className='links__button' href='https://books.zuri.team/design-rules'>Design Books
+                    <span className='tooltip'>This might be helpful too. Especially for free.</span>
+                </a>
             </div>
             <div className='social'>
                 <a className='social__icon' href='https://hng9.slack.com/team/U048L7C2KNH'><img className='social__icon' src={slack} alt='Slack link'></img>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,7 +8,11 @@ function Profile() {
       <button className='profile__share-button' type='button' aria-label='Share profile'></button>
       <img className='profile__img' id='profile__img' alt='Profile pic' src={profilepic}></img>
       <button className='profile__img-edit' type='button' aria-label='Change photo'></button>
-      <a className='profile__link' id='twitter' href='https://twitter.com/Lizazavrrr'>Liza Babicheva</a>
+      <div className='profile__links'>
+        <a id='twitter' className='profile__link' href='https://twitter.com/Lizazavrrr'>Liza Babicheva
+          <span className='tooltip'>Check out my twitter</span>
+        </a>
+      </div>
       <a className='profile__link profile__link_hide' id='slack' href='https://hng9.slack.com/team/U048L7C2KNH'>Liza Babicheva</a>
     </section>
   )
