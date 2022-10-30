89cf27ffe6c73b2edc5cf0a7b513af3716fadf89
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-30 12:04:13 +0300

    css refactoring

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,20 +6,24 @@ import github from '../images/github-logo.svg';
 function Links() {
 
     return (
-        <section id='links'>
-            <div className='buttons'>
-            <a id='btn__zuri' className='button' href='https://training.zuri.team/'>Zuri Team</a>
-            <a id='books' className='button' href='https://books.zuri.team/'>Zuri Books</a>
-            <a id='book__python' className='button' href='https://books.zuri.team/python-for-beginners?ref_id=<LizaBabicheva>'>Python Books</a>
-            <a id='pitch' className='button' href='https://background.zuri.team/'>Background Check for Coders</a>
-            <a id='book__design' className='button' href='https://books.zuri.team/design-rules'>Design Books</a>
+        <section className='links' id='links'>
+            <div className='links__buttons'>
+            <a id='btn__zuri' className='links__button' href='https://training.zuri.team/'>Zuri Team</a>
+            <a id='books' className='links__button' href='https://books.zuri.team/'>Zuri Books</a>
+            <a id='book__python' className='links__button' href='https://books.zuri.team/python-for-beginners?ref_id=<LizaBabicheva>'>Python Books</a>
+            <a id='pitch' className='links__button' href='https://background.zuri.team/'>Background Check for Coders</a>
+            <a id='book__design' className='links__button' href='https://books.zuri.team/design-rules'>Design Books</a>
             </div>
-            <div classname='social'>
-                <a href='https://github.com/LizaBabicheva' alt='Slack link'><img className='social-icon' src={slack}></img>
+            <ul className='social'>
+                <li>
+                <a href='#'><img className='social__icon' src={slack} alt='Slack link'></img>
                 </a>
-                <a href='#' alt='Github link'><img className='social-icon' src={github}></img>
+                </li>
+                <li>
+                <a href='https://github.com/LizaBabicheva'><img className='social__icon' src={github} alt='Github link'></img>
                 </a>
-            </div>
+                </li>
+            </ul>
         </section >
     )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,10 +5,10 @@ import profilepic from '../images/avatar.png';
 function Profile() {
 
   return (
-    <section id='profile'>
-      <img id='profile__img' alt='Profile pic' src={profilepic}></img>
-      <h2 id='twitter'>Liza Babicheva</h2>
-      <p id='slack'></p>
+    <section className='profile' id='profile'>
+      <img className='profile__img' id='profile__img' alt='Profile pic' src={profilepic}></img>
+      <h2 className='profile__twitter-link' id='twitter'>Liza Babicheva</h2>
+      <p className='slack__twitter-link' id='slack'></p>
     </section>
   )
 }
